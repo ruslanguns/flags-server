c824c0658370d2ed224c135695e4ad6f0d832da6
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-04-27 01:18:29 +0100

    added prisma module for core

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GraphqlModule } from './graphql/graphql.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [GraphqlModule],
-  exports: [GraphqlModule],
+  imports: [GraphqlModule, PrismaModule],
 })
 export class CoreModule {}
